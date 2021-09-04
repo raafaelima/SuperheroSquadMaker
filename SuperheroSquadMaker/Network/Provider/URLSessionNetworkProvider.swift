a16e18db4cdaad54db14b0490d18265d1899789a
Rafael Lima <Rafael.LA.Lima@ctw.bmwgroup.com>
2021-09-04 12:52:34 +0100

    Create Network provider using the NSUrlSession

---
+++ SuperheroSquadMaker/Network/Provider/URLSessionNetworkProvider.swift
@@ -0,0 +1,53 @@
+//
+//  URLSessionNetworkProvider.swift
+//  SuperheroSquadMaker
+//
+//  Created by Rafael Lima on 04/09/2021.
+//
+
+import Foundation
+
+struct URLSessionNetworkProvider: NetworkProvider {
+
+    var cacheManager: CacheManager
+    var reachability: ReachabilityType
+
+    init(reachability: ReachabilityType = Reachability(), cacheManager: CacheManager = LocalCacheManager()) {
+        self.reachability = reachability
+        self.cacheManager = cacheManager
+    }
+
+    func requestData<T: Codable>(from endpoint: Endpoint, completion: @escaping (Result<T, ApiError>) -> Void) {
+
+        let session = URLSession(configuration: URLSessionConfiguration.ephemeral, delegate: NSURLSessionPinningDelegate(), delegateQueue: nil)
+
+        let remoteDataTask = session.dataTask(with: endpoint.urlRequest()) { data, _, error in
+
+            guard let receivedData = data else {
+                completion(.failure(.emptyData))
+                return
+            }
+
+            //TODO: Define location of cache being saved
+            self.cacheManager.save(onCache: receivedData, at: "")
+            self.process(data: receivedData, completion)
+        }
+
+        if self.reachability.currentStatus() == .notReachable {
+            //TODO: Define location of cache being loaded
+            let cachedData = cacheManager.load(from: "")
+            process(data: cachedData, completion)
+        } else {
+            remoteDataTask.resume()
+        }
+    }
+
+    private func process<T: Codable>(data: Data, _ completion: @escaping (Result<T, ApiError>) -> Void) {
+        do {
+            let objectResponse = try JSONDecoder().decode(T.self, from: data)
+            completion(.success(objectResponse))
+        } catch {
+            completion(.failure(.dataParsing))
+        }
+    }
+}
