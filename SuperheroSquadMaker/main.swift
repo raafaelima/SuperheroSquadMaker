0a031c58d2cad0eba5ce82c5eebdb0023e64611c
Rafael Lima <Rafael.LA.Lima@ctw.bmwgroup.com>
2021-09-04 12:36:07 +0100

    Add slim initalization for unit tests

---
+++ SuperheroSquadMaker/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  SuperheroSquadMaker
+//
+//  Created by CTW00710-Admin on 04/09/2021.
+//
+
+import UIKit
+
+// If we have a TestingAppDelegate (i.e. we're running unit tests), use that to avoid executing initialisation code in AppDelegate
+// TheTestingAppDelegate class will only exist when running unit tests
+let appDelegateClass: AnyClass = NSClassFromString("TestingAppDelegate") ?? AppDelegate.self
+
+UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, NSStringFromClass(appDelegateClass))
